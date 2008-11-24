6e6f7f93e9dc5816f1e170d11543e19417d31576
lanenamery <lanenamery@021cfe6e-a482-11dd-bd46-993b494de23d>
2008-11-24 16:38:36 +0000

    U3dgame

--- trunk/.project/src/client/game/U3dgame.java
+++ trunk/.project/src/client/game/U3dgame.java
@@ -9,10 +9,19 @@ import client.manager.HudManager;
 import client.manager.TaskManager;
 import client.manager.ViewManager;
 
+import com.jme.bounding.BoundingBox;
+import com.jme.bounding.BoundingSphere;
 import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.light.PointLight;
 import com.jme.math.Vector3f;
 import com.jme.renderer.Camera;
 import com.jme.renderer.ColorRGBA;
+import com.jme.renderer.Renderer;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Sphere;
+import com.jme.scene.state.LightState;
+import com.jme.scene.state.ZBufferState;
 import com.jme.system.DisplaySystem;
 import com.jme.util.GameTaskQueue;
 import com.jme.util.GameTaskQueueManager;
@@ -26,13 +35,38 @@ public class U3dgame extends Game {
     private int readyPlayers = 0;
     private int nextPlayerId = 1;
     private String gameName;
+    
+	public U3dgame(){
+		super();
+	}
+	
+	protected void initSystem() {
+		this.display = DisplaySystem.getDisplaySystem(this.settings.getRenderer());
+		this.display.setTitle("U3d");
+		this.timer = new NanoTimer();
+		
+		this.initWindow();
+		this.initCamera();
+		this.initManagers();
+		this.initHotKeys();
+	}
 
-	@Override
+	protected void initWindow() {
+		this.display.setMinSamples(0);
+		this.display.setVSyncEnabled(false);
+		this.display.createWindow(this.settings.getWidth(), this.settings.getHeight(), this.settings.getDepth(),
+				this.settings.getFrequency(), false);
+		this.display.getRenderer().setBackgroundColor(ColorRGBA.black);
+
+	}
+
 	protected void initCamera() {
 		// Create the camera.
-		Camera camera = this.display.getRenderer().createCamera(this.display.getWidth(), this.display.getHeight());
-		camera.setFrustumPerspective(45.0f, this.display.getWidth()/this.display.getHeight(), .1f, 500);
-		Vector3f location = new Vector3f(0.0f, 0.0f, 0.0f);
+		Camera camera = this.display.getRenderer().createCamera(this.display.getWidth(),
+				this.display.getHeight());
+		camera.setFrustumPerspective(45.0f, this.display.getWidth()/this.display.getHeight(), 
+				1f, 500);
+		Vector3f location = new Vector3f(0.0f, 0.0f, 40.0f);
 		Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
 		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
 		Vector3f direction = new Vector3f(0.0f, 0.0f, -1.0f);
@@ -43,16 +77,12 @@ public class U3dgame extends Game {
 		this.display.getRenderer().setCamera(camera);
 	}
 
-	@Override
-	protected void initHotKeys() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	protected void initManagers() {
 		setEntityManager(EntityManager.getInstance());
-		setGameStateManager(GameStateManager.getInstance());
+		
+		setGameStateManager(GameStateManager.create());
+		this.gameStateManager.setActive(true);
+
 		setInputManager(InputManager.getInstance());
 		setHudManager(HudManager.getInstance());
 		setPhysicsManager(PhysicsManager.getInstance());
@@ -60,90 +90,70 @@ public class U3dgame extends Game {
 		setViewManager(ViewManager.getInstance());
 	}
 
-	@Override
-	protected void initWindow() {
-		this.display.createWindow(this.settings.getWidth(), this.settings.getHeight(), this.settings.getDepth(),
-				this.settings.getFrequency(), this.settings.isFullscreen());
-		this.display.getRenderer().setBackgroundColor(ColorRGBA.black);
-
+	protected void initHotKeys() {
+		KeyBindingManager.getKeyBindingManager().set("exit", KeyInput.KEY_ESCAPE);
 	}
 
-	@Override
-	public void updateHuds() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void cleanup() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	protected void initGame() {
 		//U3dLoginState login = new U3dLoginState();
 		//this.getGamestatemanager().attachChild(login);
         //login.setActive(true);
         //login.initialize();
-		
+	
         U3dExteriorState campus = new U3dExteriorState("U3dExteriorState");
         this.getGameStateManager().attachChild(campus);
         campus.setActive(true);
         campus.initialize();
-        
+/*       
         U3dEndState end = new U3dEndState();
         end.setActive(false);
-        this.getGameStateManager().attachChild(end);
+        this.getGameStateManager().attachChild(end);*/
 
         // login immediately with the current system time as the login name
        // TaskManager.getInstance().createTask("U3dSetupPlayerTask.class");
 	}
 
-	@Override
-	protected void initSystem() {
-		this.display = DisplaySystem.getDisplaySystem(this.settings.getRenderer());
-		this.display.setTitle("Snowman");
-		this.timer = new NanoTimer();
-		
-		this.initWindow();
-		this.initCamera();
-		this.initManagers();
-
-	}
-
-	@Override
-	protected void reinit() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void render(float arg0) {
-		
-
-	this.display.getRenderer().clearBuffers();
-	GameTaskQueueManager.getManager().getQueue(GameTaskQueue.RENDER).execute();
-
-	display.getRenderer().draw(((WorldGameState)this.gameStateManager.getChild("U3dExteriorState")).getRootNode());
-    
-	}
-	
-	@Override
 	protected void update(float arg0) {
+		this.timer.update();
 		this.intervalo = this.timer.getTimePerFrame();
+		
 		GameTaskQueueManager.getManager().getQueue(GameTaskQueue.UPDATE).execute();
+/*		
 		// Update input manager.
 		this.inputManager.update(this.intervalo);
 		// Execute tasks.
 		this.taskManager.update();
 		// Update physics.
 		this.physicsManager.update();
-		// Update the game states.
+*/		// Update the game states.
+		
 		this.gameStateManager.update(this.intervalo);
+
 		if(KeyBindingManager.getKeyBindingManager().isValidCommand("exit", false)) 
 			this.finish();
 		
 	}
 
+	protected void render(float arg0) {
+		Renderer r = this.display.getRenderer();
+		r.clearBuffers();
+		GameTaskQueueManager.getManager().getQueue(GameTaskQueue.RENDER).execute();
+		
+		this.gameStateManager.render(arg0);
+	}
+
+	protected void reinit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void updateHuds() {
+		// TODO Auto-generated method stub
+
+	}
 }
