a97c9d43a31cb3de91ae607ff2136a0239013d1f
gabriel.tandil <gabriel.tandil@021cfe6e-a482-11dd-bd46-993b494de23d>
2008-12-12 20:00:17 +0000

    estado de login y cambio al estado exterior, por ahora es solo una textura pegada via el hudManager, se esta investigando fengGui para su interactividad

--- trunk/src/client/manager/HudManager.java
+++ trunk/src/client/manager/HudManager.java
@@ -1,15 +1,23 @@
 package client.manager;
 
+import java.util.Iterator;
+
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+import com.jmex.game.state.BasicGameState;
+import com.jmex.game.state.GameStateManager;
+
 public class HudManager implements IHudManager {
 	private static HudManager instance=null;
+	private Node huds;
 	
+	public HudManager() {
+		this.huds = new Node("huds");
+	}
+
 	public static HudManager getInstance(){
 		if(instance==null) instance=new HudManager();
 		return instance;
-	}
-	
-	private HudManager() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
@@ -20,8 +28,23 @@ public class HudManager implements IHudManager {
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 
+		for (Iterator iterator =GameStateManager.getInstance().getChildren().iterator(); iterator.hasNext();) {
+			BasicGameState gs= (BasicGameState) iterator.next();
+			if (gs.isActive()){
+				gs.getRootNode().attachChild(huds);
+			}
+			else{
+				gs.getRootNode().detachChild(huds);
+			}
+		}
+
+	}
+	public void addHud(Spatial elHud){
+		huds.attachChild(elHud);
+	}
+	public void removeHud(Spatial elHud){
+		huds.detachChild(elHud);
 	}
 
 }
