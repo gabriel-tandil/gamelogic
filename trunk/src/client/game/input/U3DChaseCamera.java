8c7d2750a1850eaebdb54a99ae6bce0e02851ff8
lanenamery <lanenamery@021cfe6e-a482-11dd-bd46-993b494de23d>
2008-12-18 13:04:07 +0000

    Nuevo paquete con la cámara y el mouse y algunas modificaciones

---
+++ trunk/src/client/game/input/U3DChaseCamera.java
@@ -0,0 +1,89 @@
+package client.game.input;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+import com.jme.bounding.BoundingSphere;
+import com.jme.input.ChaseCamera;
+
+import com.jme.input.RelativeMouse;
+import com.jme.math.Vector3f;
+
+import com.jme.renderer.Camera;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+
+import com.jme.scene.shape.Sphere;
+
+
+public class U3DChaseCamera extends ChaseCamera {
+	
+	private Sphere sphere;
+	
+	public U3DChaseCamera(Camera cam, Spatial target) {
+		super(cam, target);
+	}
+
+	public U3DChaseCamera(Camera cam, Spatial target, HashMap props) {
+		super(cam, target, props);
+		
+		sphere = new Sphere("esphere",new Vector3f(),20,20,2);
+		sphere.setModelBound(new BoundingSphere());
+		sphere.updateModelBound();
+		sphere.setLocalTranslation(cam.getLocation());
+		sphere.setIsCollidable(true);
+	}
+	
+    protected void setupMouse() {
+        RelativeMouse mouse = new RelativeMouse("Mouse Input");
+        mouse.registerWithInputHandler(this);
+
+        if (mouseLook != null)
+            removeAction(mouseLook);
+
+        mouseLook = new U3DThirdPersonMouseLook(mouse, this, target);
+        addAction(mouseLook);
+    }
+    
+	public void update(float time) {
+		sphere.setLocalTranslation(cam.getLocation());
+		sphere.updateModelBound();
+		sphere.updateWorldBound();
+
+		super.update(time);
+	}
+	
+	public boolean verifyIntersection(Spatial world, Vector3f direction) {
+		boolean intersects = false;
+		LinkedList results = new LinkedList();
+		
+		if(world != null) {
+			this.calculateCollisions(world, results);
+			if(results.size() > 0){
+				intersects = true;
+				Vector3f actual = this.cam.getLocation();
+				Vector3f res = actual.add(direction.clone().mult(-1000));
+//				System.out.println(results);
+				cam.setLocation(res);
+				cam.update();
+			}
+		}
+		else
+			System.out.println("world es null");
+		return intersects;
+	}
+	
+	private void calculateCollisions(Spatial world, LinkedList results) {
+		 if (sphere.getWorldBound() != null && sphere.isCollidable()) {
+			 Node node = (Node)world;
+			 for (int i = 0; i < node.getQuantity(); i++) {
+		         if (sphere.getWorldBound().intersects(node.getChild(i).getWorldBound())) {
+		            	//results.add(node.getChild(i).getWorldBound());
+		            	results.add(node.getChild(i).getName());
+		         }
+			 }
+		 }
+	}
+
+}
---
+++ trunk/src/client/game/input/U3DThirdPersonMouseLook.java
@@ -0,0 +1,49 @@
+package client.game.input;
+
+import java.util.ArrayList;
+
+import client.game.state.U3dState;
+
+import com.jme.input.ChaseCamera;
+import com.jme.input.RelativeMouse;
+import com.jme.input.action.InputActionEvent;
+import com.jme.input.thirdperson.ThirdPersonMouseLook;
+import com.jme.math.Vector3f;
+import com.jme.scene.Spatial;
+import com.jmex.game.state.GameState;
+import com.jmex.game.state.GameStateManager;
+
+public class U3DThirdPersonMouseLook extends ThirdPersonMouseLook {
+	
+	private Vector3f oldDirection = new Vector3f();
+
+	public U3DThirdPersonMouseLook(RelativeMouse mouse, ChaseCamera camera, Spatial target) {
+		super(mouse, camera, target);
+	}
+	
+	public void performAction(InputActionEvent event) {
+		Vector3f direction = new Vector3f(this.mouse.getLocalTranslation());
+		Vector3f newDirection = direction.normalizeLocal().subtract(oldDirection);
+		newDirection = newDirection.normalize().negate();
+		
+		ArrayList<GameState> states = GameStateManager.getInstance().getChildren();
+		U3dState state = null;
+		
+		for(int i =0; i < states.size(); i++){
+			state = (U3dState)states.get(i);
+			if(state.isActive())
+				break;
+		}
+		
+		if(state != null) {
+			state.updateCamera(new Vector3f(newDirection));
+		}
+		else {
+			System.out.println(state.getName() + "no existe");
+		}
+		
+		this.oldDirection = direction;
+		
+		super.performAction(event);
+	}
+}
