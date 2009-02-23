e7eb4b6cd9bb9fe2d7ac3cdeb7a6ec6496a2a037
pablosantos82 <pablosantos82@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-02-23 21:34:37 +0000

    Correccion del metodo init del dynamic entity. completamiento de los .execute() de las tareas de comunicacion para la creacion de entidades dinamicas.

--- branches/client_v1_19022009/game/entity/DynamicEntity.java
+++ branches/client_v1_19022009/game/entity/DynamicEntity.java
@@ -1,8 +1,8 @@
 /**
-  * <code>DynamicEntity</code> extends <code>Entity</code> and implements
-  * <code>IDynamicEntity</code> to represent an actual dynamic entity in
-  * the game world.
-  */
+ * <code>DynamicEntity</code> extends <code>Entity</code> and implements
+ * <code>IDynamicEntity</code> to represent an actual dynamic entity in
+ * the game world.
+ */
 package client.game.entity;
 
 import java.io.IOException;
@@ -31,23 +31,24 @@ import com.jme.util.export.OutputCapsule;
 import com.jmex.game.state.BasicGameState;
 import com.jmex.game.state.GameStateManager;
 
-
-/** 
+/**
  * @author Critian Calomino
  * @version Created: 20-11-2008
  */
 public class DynamicEntity extends Entity implements IDynamicEntity {
-	
-	public void init(Vector3f force,float mass, Vector3f position,
-			Vector3f velocity, float  angle, String actualWorld, Skin theSkin, Vector3f thePosition) {
 
-		TaskManagerFactory.getInstance().add(new U3DAddDynamicEntityTaskFactory());
+	public void init(Vector3f force, float mass, Vector3f velocity,
+			float angle, String actualWorld, Skin theSkin, Vector3f thePosition) {
+
+		TaskManagerFactory.getInstance().add(
+				new U3DAddDynamicEntityTaskFactory());
 		U3DAddDynamicEntityTask task = (U3DAddDynamicEntityTask) TaskManager
-		.getInstance().createTask("8");
-		Node root =	((BasicGameState) GameStateManager.getInstance().getChild(actualWorld)).getRootNode();
+				.getInstance().createTask("8");
+		Node root = ((BasicGameState) GameStateManager.getInstance().getChild(
+				actualWorld)).getRootNode();
 		task.initTask(root, this, angle, position);
 		TaskManager.getInstance().enqueue(task);
-		
+
 		this.setAngle(angle);
 		this.setActualWorld(actualWorld);
 		this.setForce(force);
@@ -56,93 +57,99 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 		this.setSkin(theSkin);
 		this.setPosition(thePosition);
 	}
-	
-	protected Vector3f position;	
-	
+
+	protected Vector3f position;
+
 	public Vector3f getPosition() {
 		return position;
 	}
-	
+
 	public void setPosition(Vector3f thePosition) {
 		position = thePosition;
-		Node view = ((Node)ViewManager.getInstance().getView(this));
-		if (view!=null)
-		{
+		Node view = ((Node) ViewManager.getInstance().getView(this));
+		if (view != null) {
 			view.setLocalTranslation(thePosition);
 			ViewManager.getInstance().markForUpdate(this);
 		}
 	}
-	
+
 	private IPersonaje p;
-	
-	public void setPlayerAvatar(IPersonaje p){
-		this.p=p;
+
+	public void setPlayerAvatar(IPersonaje p) {
+		this.p = p;
 	}
-	
-	public void isMoving(boolean state, boolean running, boolean forward){
-		if (p!=null)
-			p.mover(state,running, forward);
+
+	public void isMoving(boolean state, boolean running, boolean forward) {
+		if (p != null)
+			p.mover(state, running, forward);
 	}
-	
-	/** 
+
+	/**
 	 * El angulo de la <code>DinamicEntity</code>
 	 */
 	protected float angle;
 
-	/** 
+	/**
 	 * Retorna el angulo de la <code>DinamicEntity</code>
+	 * 
 	 * @return angle de la DynamicEntity
 	 */
 	public float getAngle() {
 		return angle;
 	}
 
-	/** 
+	/**
 	 * Aplica el angulo de la DynamicEntity
-	 * @param theAngle el angulo de la DynamicEntity a aplicar.
+	 * 
+	 * @param theAngle
+	 *            el angulo de la DynamicEntity a aplicar.
 	 */
 	public void setAngle(float theAngle) {
 		angle = theAngle;
-		Node view = ((Node)ViewManager.getInstance().getView(this));
-		if (view!=null)
-		{
-			float[] angles=new float[3];
-			Vector3f ltras=view.getLocalTranslation();
+		Node view = ((Node) ViewManager.getInstance().getView(this));
+		if (view != null) {
+			float[] angles = new float[3];
+			Vector3f ltras = view.getLocalTranslation();
 			view.getLocalRotation().toAngles(angles);
-		    view.getLocalRotation().fromAngles(angles[0],angles[1]+ angle, angles[2]);
-		    view.setLocalTranslation(ltras);
+			view.getLocalRotation().fromAngles(angles[0], angles[1] + angle,
+					angles[2]);
+			view.setLocalTranslation(ltras);
 			ViewManager.getInstance().markForUpdate(this);
 		}
 	}
 
-	/** 
+	/**
 	 * El skin de la <code>DinamicEntity</code>.
 	 */
 	protected Skin skin;
 
-	/** 
+	/**
 	 * Retorna el skin de la <code>DinamicEntity</code>
+	 * 
 	 * @return skin de la DinamicEntity.
 	 */
 	public Skin getSkin() {
 		return skin;
 	}
 
-	/** 
+	/**
 	 * Aplcia el skin de la DynamicEntity
-	 * @param theSkin el skin de la DynamicEntity a aplicar.
+	 * 
+	 * @param theSkin
+	 *            el skin de la DynamicEntity a aplicar.
 	 */
 	public void setSkin(Skin theSkin) {
 		skin = theSkin;
 	}
 
-	/** 
+	/**
 	 * La masa de la <code>DynamicEntity</code>.
 	 */
 	protected Float mass;
 
 	/**
 	 * Retorna la masa de la DynamicEntity
+	 * 
 	 * @return mass de la DynamicEntity.
 	 */
 	public float getMass() {
@@ -151,7 +158,9 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 
 	/**
 	 * Aplica la masa a DynamicEntity
-	 * @param mass la masa de DynamicEntity a aplicar.
+	 * 
+	 * @param mass
+	 *            la masa de DynamicEntity a aplicar.
 	 */
 	public void setMass(float mass) {
 		this.mass = mass;
@@ -159,69 +168,81 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 
 	/**
 	 * El vector <code>Vector3f</code> de la <code>DynamicEntity</code>.
-	 **/
+	 */
 	protected Vector3f force;
 
-	/** 
+	/**
 	 * Retorna el <code>Vector3f</code> force de la <code>DynamicEntity</code>.
-	 * @return the <code>Vector3f</code> force de la <code>DynamicEntity</code>.
+	 * 
+	 * @return the <code>Vector3f</code> force de la
+	 *         <code>DynamicEntity</code>.
 	 */
 	public Vector3f getForce() {
 		return force;
 	}
 
-	/** 
+	/**
 	 * Aplica el <code>Vector3f</code> force a la <code>DynamicEntity</code>.
-	 * @param theForce <code>Vector3f</code> a aplicar.
+	 * 
+	 * @param theForce
+	 *            <code>Vector3f</code> a aplicar.
 	 */
 	public void setForce(Vector3f theForce) {
 		force = theForce;
 	}
-	
 
-	/** 
-	 * El vector <code>Vector3f</code> velocity de la <code>DynamicEntity</code>.
+	/**
+	 * El vector <code>Vector3f</code> velocity de la
+	 * <code>DynamicEntity</code>.
 	 */
 	protected Vector3f velocity;
 
-	/** 
-	 * Retorna el <code>Vector3f</code> velocity de la <code>DynamicEntity</code>.
+	/**
+	 * Retorna el <code>Vector3f</code> velocity de la
+	 * <code>DynamicEntity</code>.
+	 * 
 	 * @return <code>Vector3f</code> velocity de la <code>DynamicEntity</code>.
 	 */
 	public Vector3f getVelocity() {
 		return velocity;
 	}
 
-	/** 
-	 * Aplica un <code>Vector3f</code> velocity a la <code>DynamicEntity</code>.
-	 * @param theVelocity <code>Vector3f</code> velocity a aplicar.
+	/**
+	 * Aplica un <code>Vector3f</code> velocity a la
+	 * <code>DynamicEntity</code>.
+	 * 
+	 * @param theVelocity
+	 *            <code>Vector3f</code> velocity a aplicar.
 	 */
 	public void setVelocity(Vector3f theVelocity) {
 		velocity = theVelocity;
 	}
 
-	/** 
+	/**
 	 * El mundo actual donde esta la DynamicEntity.
 	 */
 	protected String actualWorld;
 
-	/** 
+	/**
 	 * Retorna el mundo actual dodne esta la DynamicEntity.
+	 * 
 	 * @return actualWorld el mundo actual donde esta la DynamicEntity.
 	 */
 	public String getActualWorld() {
 		return actualWorld;
 	}
 
-	/** 
+	/**
 	 * Aplica el mundo actual de la DynamicEntity.
-	 * @param theActualWorld mundo actual a aplicar a DynamicEntity.
+	 * 
+	 * @param theActualWorld
+	 *            mundo actual a aplicar a DynamicEntity.
 	 */
 	public void setActualWorld(String theActualWorld) {
 		actualWorld = theActualWorld;
 	}
-	
-	/** 
+
+	/**
 	 * Constructor de la DynamicEntity.
 	 */
 	public DynamicEntity(String id) {
@@ -229,9 +250,13 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 		this.setTipo("DynamicEntity");
 	}
 
-	/** 
-	 * Permite la exportacion de datos de <code>DynamicEntity</code> a una ubicacion especifica.
-	 * @param arg0 <code>JMEExporter</code> para establecer la ubicacion de los datos.
+	/**
+	 * Permite la exportacion de datos de <code>DynamicEntity</code> a una
+	 * ubicacion especifica.
+	 * 
+	 * @param arg0
+	 *            <code>JMEExporter</code> para establecer la ubicacion de los
+	 *            datos.
 	 */
 	public void write(JMEExporter arg0) {
 		OutputCapsule oc = arg0.getCapsule(this);
@@ -247,13 +272,16 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
-	/** 
-	 * Permite la importacion de datos de <code>DynamicEntity</code> a una ubicacion especifica.
-	 * @param arg0 <code>JMEImporter</code> para establecer la ubicacion de los datos.
+	/**
+	 * Permite la importacion de datos de <code>DynamicEntity</code> a una
+	 * ubicacion especifica.
+	 * 
+	 * @param arg0
+	 *            <code>JMEImporter</code> para establecer la ubicacion de los
+	 *            datos.
 	 */
 	public void read(JMEImporter arg0) {
 		InputCapsule ic = arg0.getCapsule(this);
@@ -265,26 +293,26 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 			this.velocity.setX(ic.readFloat("VelocityX", 0));
 			this.velocity.setY(ic.readFloat("VelocityY", 0));
 			this.velocity.setZ(ic.readFloat("VelocityZ", 0));
-			this.mass=ic.readFloat("Mass", 0);
+			this.mass = ic.readFloat("Mass", 0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
-	/** 
+	/**
 	 * Retorna la clase de la <code>DynamicEntity</code>.
+	 * 
 	 * @return clase de la <code>DynamicEntity</code>.
 	 */
 	public Class getClassTag() {
 		return this.getClass();
 	}
 
-	
-	/** 
+	/**
 	 * Retorna la identity de la DynamicEntity.
-	 *  @return identity de la DynamicEntity.
+	 * 
+	 * @return identity de la DynamicEntity.
 	 */
 	public String getId() {
 		return this.id;
@@ -299,18 +327,22 @@ public class DynamicEntity extends Entity implements IDynamicEntity {
 
 	/**
 	 * Retorna la velocidad actual de la DynamicEntity.
- 	* @param vector <code>Vector3f</code> velocity.
- 	* */
+	 * 
+	 * @param vector
+	 *            <code>Vector3f</code> velocity.
+	 */
 	public void addVelocity(Vector3f vector) {
 		this.velocity.addLocal(vector);
 	}
 
-	/** 
+	/**
 	 * Agrega la fuerza a esta entidad.
-	 * @param vector la fuerza en <code>Vector3f</code>.
+	 * 
+	 * @param vector
+	 *            la fuerza en <code>Vector3f</code>.
 	 */
 	public void addForce(Vector3f vector) {
 		this.force.addLocal(vector);
 	}
-	
+
 }
