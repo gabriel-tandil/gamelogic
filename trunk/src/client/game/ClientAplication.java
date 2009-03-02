8c0cfc9e9d81232812dc2d77cf954af6837e2a98
kikemdp <kikemdp@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-03-02 21:20:36 +0000

    JavaDoc de ClientAplication.java

--- trunk/src/client/game/ClientAplication.java
+++ trunk/src/client/game/ClientAplication.java
@@ -2,9 +2,16 @@ package client.game;
 
 
 
-
+/**
+ * Esta clase es la encargada de inicializar el juego
+ * @author kike
+ *
+ */
 public class ClientAplication {
-
+/**
+ * método principal de la aplicación
+ * @param args
+ */
 	public static void main(String[] args) {
 		U3dgame game = new U3dgame();
 		game.start();
