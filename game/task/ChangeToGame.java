dea162f4c76a6b508017095e0c3fe57c5215d250
juanf.silval <juanf.silval@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-03-03 15:26:42 +0000

    Agrego states modificados para poder mergear la parte de cambios de estados

---
+++ game/task/ChangeToGame.java
@@ -0,0 +1,25 @@
+package client.game.task;
+
+import client.game.state.U3dState;
+import client.minigame.MiniGameState;
+
+import com.jmex.game.state.GameStateManager;
+
+public class ChangeToGame extends ChangeStateTask {
+	private String proxEstado;
+
+	public ChangeToGame(String proxEstado) {
+		this.proxEstado = proxEstado;
+	}
+
+	public void execute() {
+		((MiniGameState)GameStateManager.getInstance().getChild(proxEstado)).initialize();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
