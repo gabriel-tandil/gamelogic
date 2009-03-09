a15019bf2dba46a9d2c5fb5e9d552b89d4c4d401
nicolas.e.gomez <nicolas.e.gomez@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-03-09 14:47:40 +0000

    Actualiza los ultimos cambios del HUD en el trunk al branch. Admás corrijo los XML porque con la integacion cambian las posiciones de las cosas.

--- branches/client_v1_19022009/game/controller/PlayerController.java
+++ branches/client_v1_19022009/game/controller/PlayerController.java
@@ -6,6 +6,7 @@ import client.game.entity.IDynamicEntity;
 import client.game.entity.Player;
 import client.game.task.U3DMoveCharacterTask;
 import client.game.task.U3DRotateCharacterTask;
+import client.manager.HudManager;
 import client.manager.TaskManager;
 
 import com.jme.input.KeyInput;
@@ -21,49 +22,51 @@ public class PlayerController extends Controller implements KeyInputListener {
 	@Override
 	public void updateLogic(float interpolation) {
 		Player player = (Player) this.getIDynamicEntity();
-		if (player!=null)
-		{
-			boolean forward=false;
-			boolean move=false;
-			float rot=0;
+		if (player != null) {
+			boolean forward = false;
+			boolean move = false;
+			float rot = 0;
 			if (flags.contains(KeyInput.KEY_D))
 				rot -= .025f;
 			if (flags.contains(KeyInput.KEY_A))
 				rot += .025f;
-	
-			if (flags.contains(KeyInput.KEY_S)){
-				move=true;
-				forward=false;
-			}	
-			if (flags.contains(KeyInput.KEY_W)){
-				move=true;
-				forward=true;
+
+			if (flags.contains(KeyInput.KEY_S)) {
+				move = true;
+				forward = false;
 			}
-			if(rot!=0){
+			if (flags.contains(KeyInput.KEY_W)) {
+				move = true;
+				forward = true;
+			}
+			if (rot != 0) {
 				U3DRotateCharacterTask task = (U3DRotateCharacterTask) TaskManager
-				.getInstance().createTask("2");
-	
-				task.initTask(player,rot);
+						.getInstance().createTask("2");
+
+				task.initTask(player, rot);
 				TaskManager.getInstance().enqueue(task);
 			}
 			if (move) {
 				U3DMoveCharacterTask task = (U3DMoveCharacterTask) TaskManager
 						.getInstance().createTask("1");
-	
-				boolean corriendo=flags.contains(KeyInput.KEY_LSHIFT);
-				task.initTask(player, false, forward,corriendo);
+
+				boolean corriendo = flags.contains(KeyInput.KEY_LSHIFT);
+				task.initTask(player, false, forward, corriendo);
 				TaskManager.getInstance().enqueue(task);
-				((Player)player).isMoving(true, corriendo, forward);
-			}else ((Player)player).isMoving(false, false, forward);
+				((Player) player).isMoving(true, corriendo, forward);
+			} else
+				((Player) player).isMoving(false, false, forward);
 		}
 	}
 
 	private Hashtable<Integer, Integer> flags;
 
 	public void onKey(char arg0, int arg1, boolean arg2) {
-		if (arg2)
-			flags.put(arg1, arg1);
-		else
+		if (arg2) {
+			if (!HudManager.getInstance().isMostrandoDialogo())
+				flags.put(arg1, arg1);
+		} else
 			flags.remove(arg1);
+
 	}
 }
