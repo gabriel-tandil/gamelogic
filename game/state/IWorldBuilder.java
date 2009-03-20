e47931360447d5c260a4b9592d865c2ffeb55413
ldoglioli <ldoglioli@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-03-20 03:37:15 +0000

    Agregado de JavaDoc

--- game/state/IWorldBuilder.java
+++ game/state/IWorldBuilder.java
@@ -10,28 +10,45 @@ import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import com.jme.scene.Skybox;
 
-/** 
+/**
+ * <code>XMLWorldBuilder</code> Tarea encargada
  * Interface que define el método buildWord() el cual es llamado para construir el RootNode de la implementación.
  * Cada clase implementa ésta interface podrá variar la forma de construir el mismo por ejemplo por XML
- * @author Mara
- * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ * @author Luciano Doglioli
+ * @version 1.0
  */
 public interface IWorldBuilder {
-	/** 
-	 * @param node
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+	/**
+	* Crea el mundo y lo agrega a node
+	* @param node <code>Node</code> el nodo contenedor del modelo que se carga.
+	*/
 	public void buildWorld(Node node);
-	
+	/**
+	 * Establece la posicion inicial del jugador dentro del mundo.
+	 */
 	public Vector3f getInitialPosition();
-	
+	/**
+	 * Crea las luces y las agrega al mundo
+	 * @param nodeRoot <code>Node</code> el nodo contenedor del modelo que se carga.
+	 */
 	public void buildLight(Node nodeRoot);
-	
+	/**
+	 * Crea la Camara que sigue al personaje.
+	 * @param playerView <code>DynamicView</code> vista del jugador a la cual se agrega la camara.
+	 */
 	public U3DChaseCamera buildCamera(DynamicView playerView);
-	
+	/**
+	 * Retorna la posicion del modelo.
+	 * @param point <code>Vector3f</code> posicion dle modelo.
+	 */
 	public void getTranslationPoint(Vector3f point);
-	
+	/**
+	 * Elimina el modelo de la memoria.
+	 * @param node <code>Node</code> nodo que se destruye.
+	 */
 	public void destroyWorld(Node node);
-	
+	/**
+	 * Configuracion del cielo del modelo.
+	 */
 	public Skybox setupSky();
 }
