abcaae3694219162c8694cbed1f6ef209ca74fc4
gabriel.tandil <gabriel.tandil@021cfe6e-a482-11dd-bd46-993b494de23d>
2009-03-20 02:40:51 +0000

    aca van mis javadocs

--- branches/client_v1_19022009/game/task/U3DCargandoTaskFactory.java
+++ branches/client_v1_19022009/game/task/U3DCargandoTaskFactory.java
@@ -1,14 +1,34 @@
 package client.game.task;
 
+/**
+ * <code>U3DCargandoTaskFactory</code> Proporciona la abstraccion necesaria en
+ * la creacion de tareas para mostrar al usuario el mensaje de que se esta
+ * cargando, del tipo <code>U3DCargandoTask</code>. Implementa la interfaz
+ * ITaskFactory. Por defecto asigna por identificador el valor "7"
+ * 
+ * @author Gabriel Alvarez
+ */
 public class U3DCargandoTaskFactory implements ITaskFactory {
 
-	private String id="7"; // por que son numeros los ids?
+	/**
+	 * El identificador de esta factoria
+	 */
+	private String id = "7";
 
+	/**
+	 * Crea una nueva <code>U3DCargandoTask</code>
+	 * 
+	 * @return <code>ITask</code> La tarea generada
+	 */
 	public ITask createTask() {
-		return new U3dCargandoTask();// aca puse este null porque no tiene sentido que le pase el state aca en el constructor, arreglar en el framework
+		return new U3dCargandoTask();
 	}
 
-
+	/**
+	 * Retorna el identificador unico de la factoria
+	 * 
+	 * @return <code>String</code> El identificador unico
+	 */
 	public String getId() {
 		return id;
 	}
--- branches/client_v1_19022009/game/task/U3DLoginRequestTask.java
+++ branches/client_v1_19022009/game/task/U3DLoginRequestTask.java
@@ -2,21 +2,48 @@ package client.game.task;
 
 import client.communication.GameContext;
 
+/**
+ * <code>U3DLoginRequestTask</code> Tarea encargada de invocar pedir al server
+ * el logueo del usuario, lo hace via <code>gameContext</code>. Extiende la
+ * funcionalidad de <code>Task</code>
+ * 
+ * @author Gabriel Alvarez
+ */
 public class U3DLoginRequestTask extends Task {
 
+	/**
+	 * string que mantiene el nombre del usuario
+	 */
 	private String nombre = null;
+	/**
+	 * string que mantiene la clave del usuario
+	 */
 	private String clave = null;
 
+	/**
+	 * constructor por defecto
+	 */
 	public U3DLoginRequestTask() {
 		super();
 	}
 
-	@Override
+	/**
+	 * Redefinido para la comparacion entre objetos
+	 * 
+	 * @param o
+	 *            <code>Object</code> El objeto contra el cual se compara
+	 * @return <code>boolean</code> Verdadero si los objetos son iguales
+	 */
 	public boolean equals(Object o) {
 
 		return false;
 	}
 
+	/**
+	 * metodo que realiza la ejecucion de la tarea llamando setea en el context
+	 * el nombre de usuario, la clave y luego pide el modulo de comunicaciones e
+	 * invoca el login
+	 */
 	public void execute() {
 		try {
 			GameContext.setUserName(nombre);
@@ -29,6 +56,12 @@ public class U3DLoginRequestTask extends Task {
 		}
 	}
 
+	/**
+	 * inicializa la tarea con el nombre de usuario y clave
+	 * 
+	 * @param nombre
+	 * @param clave
+	 */
 	public void initTask(String nombre, String clave) {
 		this.nombre = nombre;
 		this.clave = clave;
